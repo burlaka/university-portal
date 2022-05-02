816b8aa3ad370923c76b9b110880f9bee596a0ae
Denis <burlaka@yandex.ru>
2022-05-02 12:49:33 +0300

    Питоньчик

---
+++ catalog/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
+
+urlpatterns = [
+    path('books/', views.BookListView.as_view()),
+	path('books/<slug:pk>/', views.BookDetailView.as_view(), name='book-detail'),
+	path('authors/<slug:pk>/', views.AuthorDetailView.as_view(), name='author-detail'),
+]
+
+#urlpatterns += [
+#    path(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+#]
